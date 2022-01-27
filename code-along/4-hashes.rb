cf645a3fa40825f55e5d4b8f95cbc324da10ed06
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-01-27 02:55:01 +0000

    hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,37 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name: "Jake",
+    location: {
+        city: "Lincolnshire",
+        state: "Illinois"
+    },
+    status: "In-Class",
+    timeline: [
+        "In-Class",
+        "Take A Shower",
+        "Go To Bed"
+    ]
+}
+puts profile
+puts " "
+
+
+puts profile [:name]
+name = profile[:name]
+puts name
+puts " "
 
 # Accessing data from the hash
+puts profile[:location][:city]
+puts profile[:location][:state]
+puts " "
+
+puts profile [:timeline][1]
+puts " "
 
 # More Complex Hashes
+profile[:profession] = "Principal Consultant"
+puts profile
+puts profile[:profession]
