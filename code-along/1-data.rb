6706a4867d9f87bd3611e31769dc3f8784903664
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-01-27 01:21:23 +0000

    lab 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -30,6 +30,43 @@ puts "tacos" + 3.to_s
 
 # Variables
 
+a = 10
+b = 3
+
+puts a * b
+
+apple = 15
+
+puts apple * b
+
+food = "tacos"
+quantity = 3
+puts food * quantity
+
 # Combine strings and variables
 
+first_name = "Boba"
+greeting = "Hello, " + first_name
+puts greeting
+
+# String Interpolation
+greeting2 = "Hello, #{first_name}"
+puts greeting2
+
+puts "tacos #{3}"
+
+puts "#{food} #{quantity}"
+
+last_name = "Fett"
+puts greeting2 + last_name
+
+puts "Hello, #{first_name} #{last_name}"
+
 # String manipulation
+
+puts "Hello".reverse
+puts "Hello".length
+
+creed = "This Is The Way"
+puts creed.upcase
+puts creed.downcase
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -20,3 +20,11 @@
 # "i would like " + 5 + " tacos"
 # To rectify this, you can convert the number into a string
 # "i would like #{5} tacos"
+
+die1 = rand(1..6)
+die2 = rand(1..6)   
+puts die1
+puts die2           
+puts die1 + die2
+
+puts "die1 (#{die1}) + die2 (#{die2}) = #{die1 + die2}"
